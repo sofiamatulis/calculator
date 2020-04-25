dc9ccc60f7a3161eb421d6096ee0aa72c671167f
sofiamatulis <sosomatulis@hotmail.com>
2020-04-25 14:10:20 -0400

    feat(update): heading for h1 for ay11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import Calculator from './calculator'
-// import { ButtonLinkPrimary } from 'public-mobile-styleguide/src/Buttons/ButtonLinkPrimary';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p> Calculator</p>
+        <h1> Calculator</h1>
         <Calculator />
 
       </div>
